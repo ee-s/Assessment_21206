d4ee4c446bb8b8b6a6e60784363740c34fe63a5d
Eliza Smith <126822034+ee-s@users.noreply.github.com>
2023-03-27 08:37:02 +0000

    Commented

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-#13/03/2023
-#Eliza smith (21206@rangiorahigh.school.nz)
-#Python basics quiz for year 9's
+'''
+27/03/23
+Eliza smith (21206@rangiorahigh.school.nz)
+Python basics quiz for year 9's
+'''
+
 score = 0
 import time 
-TIME_CONT = 0.2 
+TIME_CONT = 0.5 
 question_number = 0
 
-#Functions 
+#Defines a function to ensure all answers are integers between 1 and 4
 def get_answer(question_reprint): 
-  """defines function to ensure all answers are integers  between 1 and 4"""
   valid_input = False # A boolean which will loop until our condition is met
   while not valid_input:
-    try: # this lets us test code for errors without breaking
+    try: # This lets us test code for errors without breaking
       user_answer = int(input(question_reprint)) 
       if user_answer > 0 and user_answer < 5:
         valid_input = True
@@ -19,22 +21,23 @@ def get_answer(question_reprint):
       print("Enter a number between 1 and 4 in answer(eg. 3) try again \n")
   return user_answer
 
-def play_again_func(user_name,score):
-  user_play_again=""
-  while user_play_again != "yes" and user_play_again != "no":
-      user_play_again=input("Would you like to play again? (Yes or No) \n".lower())
-      if user_play_again == ("no"):
-        print("Game Over, goodbye",user_name)
-        return False
-      if user_play_again== ("yes"):
-        if score >3:
-          print("Very well",user_name,"...")   
-        if score <4:
-          print("A good choice considering your score",user_name,"... \n")    
-        return True
+def play_again_func(user_name,score,user_play_again):  
+  if user_play_again == ("no"):
+    print("Game Over, goodbye",user_name)
+    return False
+  if user_play_again== ("yes"):
+    if score >3:
+      print("Very well",user_name,"with your high score i'm sure you'll do well!")   
+    if score <4:
+      print("A good choice considering your score",user_name,"... \n")    
+    return True
 
-#Lists 
+  while user_play_again != "yes" and user_play_again != "no":
+    user_play_again=input("Would you like to play again? (Yes or No) \n".lower())
+
+#A list of the correct answers to quiz questions
 answer_list = ([1, 2, 4, 1, 3, 3])
+#A list of the quiz's questions
 question_list = ([
 '''
 Q1. Was python named after a snake? 
@@ -130,8 +133,12 @@ while play_quiz==True:
   percent_correct= int(score/question_number *100)
   if score >3:
       print("Well done you have completed the test, your score is",score,"out of",question_number,"questions. Resulting in",percent_correct,"% of the test being answered correctly. \n")
-      play_quiz=play_again_func(user_name,score)      
+  user_play_again=input("Would you like to play again? (Yes or No) \n".lower())
+  play_quiz=play_again_func(user_name,score,user_play_again) 
+    
         
   if score <4:
-      print("You have completed the test it is advised to attempt this again due to you low score, your score is",score,"out of",question_number,"questions. Resulting in",percent_correct,"% of the test being answered correctly. \n")
-      play_quiz=play_again_func(user_name,score)    
+      print("You have completed the test it is advised to attempt this again due to you low score, your score is",score,"out of",question_number,"questions. Resulting in",percent_correct,"% of the test being answered correctly. \n")   
+      
+  user_play_again=input("Would you like to play again? (Yes or No) \n".lower())
+  play_quiz=play_again_func(user_name,score,user_play_again) 
